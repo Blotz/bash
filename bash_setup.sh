f800d7b00ddb5b1b29e7f4a9e3eaa4726a84da20
blotz <f.p.theil@gmail.com>
2021-05-27 15:07:31 +0100

    bash file

---
+++ bash_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1) cd into home dir
+cd
+
+# 2) download all of the bash files
+wget -N -q --show-progress "https://raw.githubusercontent.com/blotz/bash/main/.bashrc"
+wget -N -q --show-progress "https://raw.githubusercontent.com/blotz/bash/main/.bash_alias"
+wget -N -q --show-progress "https://raw.githubusercontent.com/blotz/bash/main/.profile"
+wget -N -q --show-progress "https://raw.githubusercontent.com/blotz/bash/main/.gitconfig"
+
+# 3) done!
+echo "all installed"
