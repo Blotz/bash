d1fa2cb9bac1a91ec2b9bb2b80f201a82f910098
Blitz <55319277+Blotz@users.noreply.github.com>
2021-05-27 17:16:09 +0100

    Update bash_setup.sh

--- bash_setup.sh
+++ bash_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 1) cd into home dir
-cd
+cd /home/$USER
 
 # 2) download all of the bash files
 wget -N -q --show-progress "https://raw.githubusercontent.com/blotz/bash/main/.bashrc"
 wget -N -q --show-progress "https://raw.githubusercontent.com/blotz/bash/main/.bash_aliases"
-wget -N -q --show-progress "https://raw.githubusercontent.com/blotz/bash/main/.profile"
 wget -N -q --show-progress "https://raw.githubusercontent.com/blotz/bash/main/.gitconfig"
 
 # 3) done!
 echo "all installed"
+source .bashrc
